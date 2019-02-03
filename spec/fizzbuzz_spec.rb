bb6489bfc2e009f1e6714f9f694eea4237b56024
darling-rockstar-4852 <daisyrene483@gmail.com>
2019-02-03 16:27:26 +0000

    Automatically backed up by Learn

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,6 +1,6 @@
 require_relative './spec_helper.rb'
 require_relative '../fizzbuzz.rb'
-
+`require "spec_helper"`
 
 describe "fizzbuzz" do
   it 'returns "Fizz" when the number is divisible by 3' do
